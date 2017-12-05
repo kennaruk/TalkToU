18af7ba81b57a4c1c1686343e08364deeebab637
Nitipat Wuttisasiwat <k-e-n_ken@hotmail.com>
2017-12-06 01:31:04 +0700

    refractor code

--- main.py
+++ main.py
@@ -3,98 +3,113 @@ import sys
 import threading
 from authen import *
 
-class ThreadedClient:
-    listenSocket = socket.socket(
-        socket.AF_INET, socket.SOCK_STREAM)
+class SocketConnection:
     serverSocket = socket.socket(
         socket.AF_INET, socket.SOCK_STREAM)
 
     def __init__(self, host, port):
         #Server Connect
         self.serverConnect(host, port)
-        
-        #Listen
-        self.listeningThread()
-        
-
-    def listeningThread(self):
-        #listenSocket
-        self.listenSocket.bind((CLIENT['IP'], (int)(CLIENT['PORT']) ))
-        self.listenSocket.listen(1)
-        while True:
-            (clientSocket, address) = self.listenSocket.accept()
-            print ("Connection from", address)
-
-            # print ("Thread Connection")
-            threading.Thread(target = self.connectionThread(clientSocket, address)).start()
-
-    def connectionThread(self, socket, address):
-        self.send(socket, "Type 'exit' to end the connection.\n")
-        threading.Thread(target = self.printChat, args = (socket, address)).start()   
-        threading.Thread(target = self.sendChat, args = (socket, )).start()
-        # while True:
-        #     text = input()
-        #     # print ('You:\n', text)
-        #     self.send(socket, text)     
-
-    def sendChat(self, socket):
-        while True:
-            text = sys.stdin.readline()
-            print ('You:\n', text)
-            self.send(socket, text)
-            
-
-    def printChat(self, sock, address):
-        while True:
-            recvChat = self.recv(sock)
-            if not recvChat:
-                sock.shutdown(socket.SHUT_WR)
-            # print(':'+recvChat+':')
-            # print(recvChat.split('\n'))
-            if recvChat.split('\n')[0] != 'exit\r':
-                print(address, ":\n", recvChat)     
-            else:
-                print('Connection closed.')
-                sock.close()
-                break
-                
 
     def serverConnect(self, host, port):
         self.serverSocket.connect((host, port))
-        self.send(self.serverSocket, AUTHEN)
-        # print("Server Status:")
-        serverStatus = self.serverSocket.recv(4096).decode() #Server Status
-        print('Connected server status: ', serverStatus)
+        self.serverAuthen()
+        self.serverStatusCheck()
+        self.serverRcvFriendList()
+        
+        threading.Thread(target = self.heartbeat).start()
 
-        # print("Friend List:")
-        friendListRecieve = self.recv(self.serverSocket) #FriendList
+        threading.Thread(target = self.listen).start()
 
-        print('Friend List:')
-        self.friendList = []
-        for friend in friendListRecieve.split('\n'):
+    def serverAuthen(self):
+        self.sockSend(self.serverSocket, AUTHEN)
+
+    def serverStatusCheck(self):
+        serverConStatus = self.sockRecv(self.serverSocket).strip(' \n')
+        if serverConStatus == '200 SUCCESS':
+            print ('Server connected.')
+        else:
+            print ('Error while connecting server.')
+            sys.exit(-1)
+    
+    def serverRcvFriendList(self):
+        friendListRecv = self.sockRecv(self.serverSocket)
+        self.printFriendList(friendListRecv)
+
+    def printFriendList(self, friendList):
+        print ('\nFriends list:\n')
+        for friend in friendList.split('\n'):
             if friend != 'END' and friend != '':
-                self.friendList.append(friend)
                 print(friend)
         print()
-                # print(friend.split(':')[0])
 
-        # print("Heartbeat Threading!")
-        threading.Thread(target = self.heartbeatThread).start() #Thread HeartBeat
-
-    def heartbeatThread(self):
+    def heartbeat(self):
         while True:
             greetingMsg = "Hello " + CLIENT['USER']
-            serverRcv = self.recv(self.serverSocket)
+            serverRcv = self.sockRecv(self.serverSocket)
             if greetingMsg == serverRcv:
-                # print(serverRcv)
-                # print("Hello Server")
-                self.send(self.serverSocket, "Hello Server")
+                self.sockSend(self.serverSocket, "Hello Server")
+    
+    def listen(self):
+        listenSocket = socket.socket(
+        socket.AF_INET, socket.SOCK_STREAM)
 
-    def send(self, socket, msg):
-        socket.send(bytes(msg, 'utf-8'))
+        listenSocket.bind((CLIENT['IP'], (int)(CLIENT['PORT']) ))
+        listenSocket.listen(1)
+        while True:
+            (connectionSocket, address) = listenSocket.accept()
+            print ("Connection from", address)
+            threading.Thread(target = self.connection, args = (connectionSocket, address)).start()
+    
+    def connection(self, sock, address):
+        self.sockSend(sock, "Type 'exit' to end the connection.\n")
+        sendMsgThread = threading.Thread(target = self.sendMsg, args = (sock, )).start()
+        
+        threading.Thread(target = self.recvMsg, args = (sock, address)).start()   
 
-    def recv(self, socket):
-        recv = socket.recv(4096).decode()
-        return recv
+    def sendMsg(self, sock):
+        while True:
+            try:
+                send_msg = sys.stdin.readline()
+                print()
+                send_msg = CLIENT['IP'] + ":\n" + send_msg
+                self.sockSend(sock, send_msg)
+            except BrokenPipeError:            
+                return
+    
+    def recvMsg(self, sock, address):
+        while True:
+            try:
+                recv_msg = self.sockRecv(sock)
+                if not recv_msg:
+                    sock.shutdown(socket.SHUT_WR)
+                if recv_msg.split('\n')[0] != 'exit\r':
+                    print(address, ":\n", recv_msg)     
+                else:
+                    print('Connection closed.')
+                    sock.shutdown(socket.SHUT_WR)
+                    break
+            except UnicodeDecodeError:
+                print('Connection closed.')                
+                sock.shutdown(socket.SHUT_WR)
+            except TypeError:
+                print('Connection closed.')                
+                sock.shutdown(socket.SHUT_WR)
+            
+    def sockSend(self, sock, msg):
+        try:
+            sock.send(bytes(msg, 'utf-8'))
+        except BrokenPipeError:
+            raise BrokenPipeError
 
-client = ThreadedClient("128.199.83.36", 34260)
+    def sockRecv(self, sock):
+        try:
+            recvMsg = sock.recv(4096).decode()
+            return recvMsg
+        except UnicodeDecodeError:
+            raise UnicodeDecodeError
+        except TypeError:
+            raise TypeError
+
+#initiate application
+TUSocket = SocketConnection("128.199.83.36", 34260)
